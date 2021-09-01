81efd0e939472ecbf16846f3bfec24cf6549e82c
David <74916391+DavidShulga@users.noreply.github.com>
2021-09-01 18:05:00 +0300

    Changes

---
+++ qapi.js
@@ -0,0 +1,31 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"612360677514e1001f258544",
+				"612360677514e1001f258546"
+			]
+		},
+		"612360677514e1001f258544": {
+			"id": "612360677514e1001f258544",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"612360677514e1001f258546": {
+			"id": "612360677514e1001f258546",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {
+			"display": ""
+		},
+		"seo": {}
+	}
+}
---
+++ src/pages/index.js
@@ -0,0 +1,68 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Box, Button, Hr, Icon, Image, Input, Text, List, Section } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { GlobalQuarklyPageStyles } from "global-page-styles";
+import { RawHtml } from "@quarkly/components";
+import * as Components from "components";
+import { MdFace } from "react-icons/md";
+export default (() => {
+	return <Theme theme={theme}>
+		<GlobalQuarklyPageStyles pageUrl={"index"} />
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Box />
+		<Button>
+			Button
+		</Button>
+		<Hr />
+		<Icon category="md" icon={MdFace} />
+		<Image width="64px" height="64px" />
+		<Input />
+		<Link href="#">
+			Some text
+		</Link>
+		<List>
+			<Text>
+				First item
+			</Text>
+		</List>
+		<Section />
+		<Text>
+			Some text
+		</Text>
+		<Components.QuarklycommunityKitLottie forceUpdate isStopped={false} isPaused={false} />
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+		<RawHtml>
+			<style place={"endOfHead"} rawKey={"612360677514e1001f25853f"}>
+				{":root {\n  box-sizing: border-box;\n}\n\n* {\n  box-sizing: inherit;\n}"}
+			</style>
+		</RawHtml>
+	</Theme>;
+});
